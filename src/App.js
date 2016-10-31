48b0b319413670c48caae76a445259f6988a1364
Alex <alex.frieder@gmail.com>
2016-10-31 13:28:38 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ const STIM_SIZE = 50 // px
 const GRID_SIZE = 64 // scalar
 const VISUAL_ANGLE = 11 // px
 const NUM_BLOCKS = 1 // 1-indexed because I suck
-const NUM_TRIALS_PER_BLOCK = 3 // 0-indexed because I suck
+const NUM_TRIALS_PER_BLOCK = 2 // 0-indexed because I suck
 const MIN_SCREENS = 4
 const MAX_SCREENS = 14
 
